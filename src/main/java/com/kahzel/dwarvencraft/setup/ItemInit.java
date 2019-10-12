41aecca4b39db0a96fe0206b70bc53bd2cd1634b
Kahzel <iamepicgod@gmail.com>
2019-10-12 11:53:47 -0300

    refactor(assets): finished chunk, ingot and sand textures

--- src/main/java/com/kahzel/dwarvencraft/setup/ItemInit.java
+++ src/main/java/com/kahzel/dwarvencraft/setup/ItemInit.java
@@ -10,19 +10,19 @@ public class ItemInit {
     private static final Setup SETUP = DwarvenCraft.SETUP;
 
     public static void init(final RegistryEvent.Register<Item> itemRegistryEvent) {
-        Item.Properties blockProps = new Item.Properties().group(SETUP.itemGroup);
+        Item.Properties baseProps = new Item.Properties().group(SETUP.itemGroup);
         Item.Properties sandProps = new Item.Properties().group(SETUP.itemGroup).maxStackSize(96);
 
         //BlockItem setup
-        itemRegistryEvent.getRegistry().register(new BlockItem(BlockInit.MYTHRITE_ORE, blockProps)
+        itemRegistryEvent.getRegistry().register(new BlockItem(BlockInit.MYTHRITE_ORE, baseProps)
                 .setRegistryName("mythriteoreblock"));
-        itemRegistryEvent.getRegistry().register(new BlockItem(BlockInit.MALACHITE_ORE, blockProps)
+        itemRegistryEvent.getRegistry().register(new BlockItem(BlockInit.MALACHITE_ORE, baseProps)
                 .setRegistryName("malachiteoreblock"));
-        itemRegistryEvent.getRegistry().register(new BlockItem(BlockInit.TUNGSTEN_ORE, blockProps)
+        itemRegistryEvent.getRegistry().register(new BlockItem(BlockInit.TUNGSTEN_ORE, baseProps)
                 .setRegistryName("tungstenoreblock"));
-        itemRegistryEvent.getRegistry().register(new BlockItem(BlockInit.COPPER_ORE, blockProps)
+        itemRegistryEvent.getRegistry().register(new BlockItem(BlockInit.COPPER_ORE, baseProps)
                 .setRegistryName("copperoreblock"));
-        itemRegistryEvent.getRegistry().register(new BlockItem(BlockInit.TIN_ORE, blockProps)
+        itemRegistryEvent.getRegistry().register(new BlockItem(BlockInit.TIN_ORE, baseProps)
                 .setRegistryName("tinoreblock"));
 
         //Item setup
@@ -34,16 +34,21 @@ public class ItemInit {
         itemRegistryEvent.getRegistry().register(new Item(sandProps).setRegistryName("ironsand"));
         itemRegistryEvent.getRegistry().register(new Item(sandProps).setRegistryName("goldsand"));
 
-        itemRegistryEvent.getRegistry().register(new Item(blockProps).setRegistryName("myhtritechunk"));
-        itemRegistryEvent.getRegistry().register(new Item(blockProps).setRegistryName("malachitechunk"));
-        itemRegistryEvent.getRegistry().register(new Item(blockProps).setRegistryName("tungstenchunk"));
-        itemRegistryEvent.getRegistry().register(new Item(blockProps).setRegistryName("copperchunk"));
-        itemRegistryEvent.getRegistry().register(new Item(blockProps).setRegistryName("tinchunk"));
-        itemRegistryEvent.getRegistry().register(new Item(blockProps).setRegistryName("ironchunk"));
-        itemRegistryEvent.getRegistry().register(new Item(blockProps).setRegistryName("goldchunk"));
-        itemRegistryEvent.getRegistry().register(new Item(blockProps).setRegistryName("redstonechunk"));
+        itemRegistryEvent.getRegistry().register(new Item(baseProps).setRegistryName("mythritechunk"));
+        itemRegistryEvent.getRegistry().register(new Item(baseProps).setRegistryName("malachitechunk"));
+        itemRegistryEvent.getRegistry().register(new Item(baseProps).setRegistryName("tungstenchunk"));
+        itemRegistryEvent.getRegistry().register(new Item(baseProps).setRegistryName("copperchunk"));
+        itemRegistryEvent.getRegistry().register(new Item(baseProps).setRegistryName("tinchunk"));
+        itemRegistryEvent.getRegistry().register(new Item(baseProps).setRegistryName("ironchunk"));
+        itemRegistryEvent.getRegistry().register(new Item(baseProps).setRegistryName("goldchunk"));
+        itemRegistryEvent.getRegistry().register(new Item(baseProps).setRegistryName("redstonechunk"));
 
-        itemRegistryEvent.getRegistry().register(new Item(blockProps).setRegistryName("malachiteingot"));
+        itemRegistryEvent.getRegistry().register(new Item(baseProps).setRegistryName("malachiteingot"));
+        itemRegistryEvent.getRegistry().register(new Item(baseProps).setRegistryName("mythriteingot"));
+        itemRegistryEvent.getRegistry().register(new Item(baseProps).setRegistryName("tungsteningot"));
+        itemRegistryEvent.getRegistry().register(new Item(baseProps).setRegistryName("copperingot"));
+        itemRegistryEvent.getRegistry().register(new Item(baseProps).setRegistryName("tiningot"));
+
 
 
     }
